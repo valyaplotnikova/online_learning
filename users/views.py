bed663bfde454a9ec0c0795f0130fde08e3ce205
valya_plotnikova <valya_ezhova@mail.ru>
2024-09-20 15:50:28 +0300

    update views users

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
-
-from users.models import User
-from users.serializers import UserSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
+from users.models import User, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentsViewSet(viewsets.ModelViewSet):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['paid_course', 'paid_lesson', 'payment_method']
+    ordering_fields = ['payment_data']
