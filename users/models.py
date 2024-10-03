855a9f13ca1f4777d726392ad5150322889c3326
valya_plotnikova <valya_ezhova@mail.ru>
2024-10-03 15:15:19 +0300

    update users

--- users/models.py
+++ users/models.py
@@ -5,12 +5,15 @@ from phonenumber_field.modelfields import PhoneNumberField
 from materials.models import Course, Lesson
 
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email')
-    phone = PhoneNumberField(blank=True)
-    city = models.CharField(max_length=35, blank=True, null=True, verbose_name='Город')
-    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, verbose_name='Аватар')
+    phone = PhoneNumberField(**NULLABLE)
+    city = models.CharField(max_length=35, **NULLABLE, verbose_name='Город')
+    avatar = models.ImageField(upload_to='users/avatars/', **NULLABLE, verbose_name='Аватар')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -29,14 +32,17 @@ class Payments(models.Model):
         ('перевод на счет', 'перевод на счет')
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
-    payment_data = models.DateTimeField(verbose_name='дата оплаты')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, verbose_name='пользователь')
+    payment_data = models.DateTimeField(**NULLABLE, verbose_name='дата оплаты')
+    amount = models.PositiveIntegerField(verbose_name='сумма оплаты')
     paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL,
-                                    blank=True, null=True, verbose_name='оплаченный курс')
+                                    **NULLABLE, verbose_name='оплаченный курс')
     paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL,
-                                    blank=True, null=True, verbose_name='оплаченный урок')
-    payment_sum = models.IntegerField(default=0, verbose_name='сумма оплаты')
+                                    **NULLABLE, verbose_name='оплаченный урок')
     payment_method = models.CharField(max_length=50, choices=METHOD, verbose_name='способ оплаты', default='наличные')
+    session_id = models.CharField(max_length=255, verbose_name='ID сессии', **NULLABLE)
+    link_to_payment = models.URLField(max_length=400, verbose_name='ссылка на оплату', **NULLABLE)
+    payment_status = models.CharField(max_length=400, verbose_name='ссылка на оплату', **NULLABLE)
 
     def __str__(self):
         return f'{self.user} - {self.paid_course if self.paid_course else self.paid_lesson} ({self.payment_sum})'
--- users/permissions.py
+++ users/permissions.py
@@ -2,13 +2,13 @@ from rest_framework import permissions
 
 
 class IsModers(permissions.BasePermission):
-    """Проверяет, является ли пользователь Модератором."""
+    """ Проверяет, является ли пользователь Модератором. """
     def has_permission(self, request, view):
         return request.user.groups.filter(name='moders').exists()
 
 
 class IsOwner(permissions.BasePermission):
-    """Проверяет, является ли пользователь владельцем."""
+    """ Проверяет, является ли пользователь владельцем. """
     def has_object_permission(self, request, view, obj):
 
         if obj.owner == request.user:
